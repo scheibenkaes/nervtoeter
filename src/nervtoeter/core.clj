cbf9394280390e7bee71dba0061993f5d04200f6
Benjamin Klüglein <scheibenkaes@googlemail.com>
2012-09-23 20:26:54 +0200

    final layout

--- src/nervtoeter/core.clj
+++ src/nervtoeter/core.clj
@@ -1,40 +1,52 @@
 (ns nervtoeter.core
   (:use [jayq.core :only [$ append]]))
 
-(def sounds
+(def ^:export sounds
   (sorted-map
    "Anfang"
    [["Cowboy" "sound/Cowboy_Theme-Pavak-1711860633.mp3"]]
-   "Gesammelt"
+   "Lachen"
+   [["Babylachen" "sound/Kid_Laugh-Mike_Koenig-1673908713.mp3"]
+    ["Lachen" "sound/Laughter-Mike_Koenig-360558723.mp3"]
+    ["Hahaha" "sound/hahaha-Peter_De_Lang-1639076107.mp3"]
+    ]
+   "Positiv"
    [
     ["Applaus" "sound/5_Sec_Crowd_Cheer-Mike_Koenig-1562033255.mp3"]
-    ["Horn" "sound/Air Horn-SoundBible.com-964603082.mp3"]
-    ["Buzzer" "sound/Buzzer-SoundBible.com-188422102.mp3"]
-    ["Kasse" "sound/Cha_Ching_Register-Muska666-173262285.mp3"]
-    ["No" "sound/Hl2_Rebel-Ragdoll485-573931361.mp3"]
-    ["Babylachen" "sound/Kid_Laugh-Mike_Koenig-1673908713.mp3"]
-    ["Voegel" "sound/Laughing_kookaburra_birds-Christopher-718473240.mp3"]
-    ["Lachen" "sound/Laughter-Mike_Koenig-360558723.mp3"]
     ["Ovationen" "sound/Ovation-Mike_Koenig-1061486511.mp3"]
+    ["Voegel" "sound/Laughing_kookaburra_birds-Christopher-718473240.mp3"]
     ["Hahn" "sound/Rudy_rooster_crowing-Shelley-1948282641.mp3"]
+    ["Horn" "sound/Air Horn-SoundBible.com-964603082.mp3"]
+    ["Kasse" "sound/Cha_Ching_Register-Muska666-173262285.mp3"]
+    ["Truthahn" "sound/Turkey Gobble-SoundBible.com-1935194723.mp3"]
+
+    ]
+
+   "Negativ"
+   [
+    ["No" "sound/Hl2_Rebel-Ragdoll485-573931361.mp3"]
     ["Sad Trombone" "sound/Sad_Trombone-Joe_Lamb-665429450.mp3"]
-    ["Slap" "sound/Slap-SoundMaster13-49669815.mp3"]
     ["Schleim" "sound/Slime-SoundBible.com-803762203.mp3"]
     ["Punch" "sound/Strong_Punch-Mike_Koenig-574430706.mp3"]
-    ["Truthahn" "sound/Turkey Gobble-SoundBible.com-1935194723.mp3"]
-    ["Flyby" "sound/flyby-Conor-1500306612.mp3"]
-    ["Hahaha" "sound/hahaha-Peter_De_Lang-1639076107.mp3"]
+    ["Buzzer" "sound/Buzzer-SoundBible.com-188422102.mp3"]
+    ]
+
+   "Kampf"
+   [
     ["Klingen" "sound/rage_of_blades-Blaga_Saun-1763516257.mp3"]
     ["Donner" "sound/thunder_strike_1-Mike_Koenig-739781745.mp3"]
+    ["Slap" "sound/Slap-SoundMaster13-49669815.mp3"]
+    ["Flyby" "sound/flyby-Conor-1500306612.mp3"]
+    
     ]))
 
-(defn find-sound [group sound]
+(defn ^:export find-sound [group sound]
   (when-let [gr (get sounds group)]
     (first (for [s gr :when (= sound (first s))] (second s)))))
 
-(def audio ($ :#player))
+(def ^:export audio ($ :#player))
 
-(def player (atom nil))
+(def ^:export player (atom nil))
 
 (defn ^:export init-player []
   (let [pl (js/Audio. "")]
@@ -47,7 +59,7 @@
 (defn ^:export play-button [group txt sample]
   (str "<li><a onclick=\"nervtoeter.core.play_sound('" group "','" txt "')\">" txt "</a></li>"))
 
-(defn create-buttons [group sounds]
+(defn ^:export create-buttons [group sounds]
   (for [s sounds] (play-button group (first s) (second s))))
 
 (defn ^:export create-lis []
