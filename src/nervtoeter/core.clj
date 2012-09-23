213f701329ce5d8419b89e2054fcdabc1ef6e9ee
Benjamin Klüglein <scheibenkaes@googlemail.com>
2012-09-23 15:16:57 +0200

    simple mp3 playback

--- src/nervtoeter/core.clj
+++ src/nervtoeter/core.clj
@@ -1,10 +1,27 @@
 (ns nervtoeter.core
-  (:use [jayq.core :only [$]]))
+  (:use [jayq.core :only [$ append]]))
 
-(def $jplayer ($ "#jplayer"))
+(def sounds
+  {"ovation" "sound/ovation.mp3"})
+
+(def audio ($ :#player))
+
+(def player (atom nil))
+
+(defn ^:export init-player []
+  (let [pl (js/Audio. "")]
+    (append audio pl)
+    (reset! player pl)))
+
+(init-player)
 
 (defn ^:export play-sound 
   ""
   [sound]
-  (js/alert sound))
+  (if-let [s (get sounds sound)]
+    (do
+      (set! (.-src @player) s)
+      (.log js/console @player)
+      (. @player (play)))
+    (js/alert "Sound nicht gefunden")))
 
