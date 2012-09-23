604bfa8b0605cec5c14273a5225d19d864373732
Benjamin Klüglein <scheibenkaes@googlemail.com>
2012-09-23 16:40:32 +0200

    Generating buttons

--- src/nervtoeter/core.clj
+++ src/nervtoeter/core.clj
@@ -2,8 +2,11 @@
   (:use [jayq.core :only [$ append]]))
 
 (def sounds
-  {"Beginn" 
-   [["ovation" "sound/ovation.mp3"]]})
+  {
+   "Beginn" 
+   [["Ovation" "sound/ovation.mp3"]]
+   "Sieg"
+   [["Ovation" "sound/ovation.mp3"]]})
 
 (defn find-sound [group sound]
   (when-let [gr (get sounds group)]
@@ -18,7 +21,34 @@
     (append audio pl)
     (reset! player pl)))
 
-($ init-player)
+(defn ^:export divider [txt]
+  (str "<li data-role='list-divider'>" txt "</li>"))
+
+(defn ^:export play-button [group txt sample]
+  (str "<li><a onclick=\"nervtoeter.core.play_sound('" group "','" txt "')\">" txt "</a></li>"))
+
+(defn create-buttons [group sounds]
+  (for [s sounds] (play-button group (first s) (second s))))
+
+(defn ^:export create-lis []
+  (reduce (fn [acc [group snds]]
+            (let [div (divider group)
+                  btns (create-buttons group snds)] 
+              (concat acc [div] btns))
+            ) [] sounds))
+
+(defn ^:export setup-ul []
+  (let [lis (create-lis)
+        ul ($ :#ul)]
+    (doseq [li lis]
+      (append ul li))
+    (.listview ul "refresh")))
+
+(defn ^:export init-app [& _]
+  (init-player)
+  (setup-ul))
+
+($ init-app)
 
 (defn ^:export play-sound 
   ""
@@ -27,5 +57,7 @@
     (do
       (set! (.-src @player) s)
       (. @player (play)))
-    (js/alert "Sound nicht gefunden")))
+    (do
+      (.log js/console (str "Gr: " group " S: " sound))
+      (js/alert "Sound nicht gefunden"))))
 
